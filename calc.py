c47318c9c2631541859ee4cf3a0b2e3a756ada6f
shobhitbehl <shobhitbehl@gmail.com>
2020-05-09 23:40:19 +0530

    Added subtract

--- calc.py
+++ calc.py
@@ -1,9 +1,24 @@
+import logging
+
 def add(a, b):
     return a + b
+
+def sub(a, b):
+    return a - b
 
 if __name__ == "__main__":
     oprnd = "+";
     a = 2
     b = 3
     if oprnd == "+":
-        print(add(a, b))
+        summ = add(a, b)
+
+    oprnd = "-"
+    if oprnd == "-":
+        diff = sub(a, b)
+
+    logging.basicConfig(filename="logFile.txt",
+                filemode='a',
+                format='%(asctime)s %(levelname)s-%(message)s',
+                datefmt='%Y-%m-%d %H:%M:%S',level=logging.DEBUG)
+    logging.info(summ)
--- test_calc.py
+++ test_calc.py
@@ -1,4 +1,7 @@
 from calc import *
 
 def test_add():
-    assert add(5,3) == 8
+    assert add(5, 3) == 8
+
+def test_sub():
+    assert sub(5, 3) == 2
